bc499d29c3ed177349cba506be81557b0f6faa49
Ruvenss <ruvenss@gmail.com>
2018-05-26 12:28:43 +0200

    Initial data

---
+++ config_sample.php
@@ -0,0 +1,33 @@
+<?php
+// Main and General
+$underconstruction = true;
+$draftsite         = ".sigma.ink";
+$publicsite        = "";
+$maintitle         = "";
+// sqlite config 
+$dbkey             = "p3gf31dk-st04-rp41-u0ca-s9321a0f4vmt";
+// MariaDB Config
+$dbhost			   = "127.0.0.1";
+$dbuser			   = "";
+$dbpswd            = "";
+$dbname            = "";
+// Communication tools
+$mailChimp_ApiKey  = "";	// http://kb.mailchimp.com/article/where-can-i-find-my-api-key
+$mailChimp_ListId  = "";	// http://kb.mailchimp.com/article/how-can-i-find-my-list-id/
+$smstoken		   = "";
+$emailnotify	   = "";
+$emailname		   = "";
+$emailtoken		   = "";
+$emailpassword	   = "";
+$slackurl		   = "";
+$slacktoken		   = "";
+$slackchannel	   = "";
+// Google
+$g_captcha		   = "";
+$g_analitycs_id	   = "";
+$g_maps_key		   = "";
+$g_calendar_key	   = "";
+// Internalisation i18
+$language		   = "en_GB";
+$languages		   = ["en","fr"];
+?>
