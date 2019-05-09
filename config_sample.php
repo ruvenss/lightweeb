e86b3de00b249b30ae232c901000a3e87ffb2d09
Ruvenss G. Wilches <ruvenss@gmail.com>
2019-05-09 19:44:29 +0200

    Update config_sample.php

--- config_sample.php
+++ config_sample.php
@@ -35,26 +35,60 @@ $languages		   = ["en","fr"];
 $searchfooter      = "";
 // PWA
 $manifest         = '{
-  "name": "Appname",
-  "short_name": "Appname",
-  "theme_color": "#2196f3",
-  "background_color": "#2196f3",
-  "display": "fullscreen",
-  "orientation": "portrait",
-  "scope": "/",
-  "start_url": "/",
-  "icons": [{
-					"src": "/img/icon-96.png",
-					"sizes": "96x96",
-					"type": "image/png"
-				},{
-					"src": "/img/icon-144.png",
-					"sizes": "144x144",
-					"type": "image/png"
-				},{
-					"src": "/img/icon-192.png",
-					"sizes": "192x192",
-					"type": "image/png"
-	}]
+"name": "Appname",
+"short_name": "Appname",
+"theme_color": "#2196f3",
+"background_color": "#2196f3",
+"display": "standalone",
+"orientation": "portrait",
+"scope": "/",
+"start_url": "/",
+"icons": [
+        {
+            "src": "/images/icons/android-icon-36x36.png",
+            "sizes": "36x36",
+            "type": "image/png"
+        },
+        {
+            "src": "/images/icons/android-icon-48x48.png",
+            "sizes": "48x48",
+            "type": "image/png"
+        },
+        {
+            "src": "/images/icons/android-icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png"
+        },
+        {
+            "src": "/images/icons/android-icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+        },
+        {
+            "src": "/images/icons/android-icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+        },
+        {
+            "src": "/images/icons/android-icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "/images/icons/android-icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+        },
+        {
+            "src": "/images/icons/android-icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+        },
+        {
+            "src": "/images/icons/android-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ]
 }';
 ?>
