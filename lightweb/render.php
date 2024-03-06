ae3fe94c41e84b398f0ecfe5c102cb0c77e419c7
Ruvenss G. Wilches <ruvenss@gmail.com>
2024-03-06 23:51:34 +0100

    update

--- lightweb/render.php
+++ lightweb/render.php
@@ -1,5 +1,17 @@
 <?php
-function render_page($page)
+function render_page($page = "home")
+{
+    if (isset(LIGHTWEB_TREE[$page])) {
+        $headerhtml = file_get_contents(LIGHTWEB_PAGES_HEADERS_PATH . LIGHTWEB_TREE[$page]['header']);
+        $bodyhtml = file_get_contents(LIGHTWEB_PAGES_PATH . LIGHTWEB_TREE[$page]['path']);
+        $footerhtml = file_get_contents(LIGHTWEB_PAGES_FOOTERS_PATH . LIGHTWEB_TREE[$page]['footer']);
+
+        return($headerhtml . "\n" . $bodyhtml . "\n" . $footerhtml);
+    } else {
+        render_404();
+    }
+}
+function render_404()
 {
 
 }
