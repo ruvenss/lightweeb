20e81b4ed0f71d5e63ba4112dbfaa2e7dc1da673
Ruvenss G. Wilches <ruvenss@gmail.com>
2024-03-07 08:26:13 +0100

    update

--- lightweb/publish.php
+++ lightweb/publish.php
@@ -30,4 +30,26 @@ function prepare_render()
 Options -Indexes
 ErrorDocument 404 https://" . LIGHTWEB_PRODUCTION . "/" . LIGHTWEB_LANG . "/404";
     file_put_contents(LIGHTWEB_PUBLISH_PATH . "uncompress/.htaccess", $htaccess);
+    $configphp = '<?php' . "\n";
+    if (LIGHTWEB_DB) {
+        $configphp .= "define('LIGHTWEB_DB_HOST', '" . LIGHTWEB_DB_HOST . "');
+define('LIGHTWEB_DB_USER', '" . LIGHTWEB_DB_USER . "');
+define('LIGHTWEB_DB_PASS', '" . LIGHTWEB_DB_PASS . "');
+define('LIGHTWEB_DB_NAME', '" . LIGHTWEB_DB_NAME . "');
+define('LIGHTWEB_DB_PREFIX', '" . LIGHTWEB_DB_PREFIX . "');
+define('LIGHTWEB_DB_PORT', '" . LIGHTWEB_DB_PORT . "');
+define('LIGHTWEB_DB_CHARSET', '" . LIGHTWEB_DB_CHARSET . "');
+define('LIGHTWEB_DB_COLLATE', '" . LIGHTWEB_DB_COLLATE . "');";
+    }
+    file_put_contents(LIGHTWEB_PUBLISH_PATH . "uncompress/api/config.php", $configphp);
+    if (!file_exists(LIGHTWEB_PUBLISH_PATH . "uncompress/api/locales")) {
+        mkdir(LIGHTWEB_PUBLISH_PATH . "uncompress/api/locales");
+    }
+    $locales_files = scandir(LIGHTWEB_LOCALES_PATH);
+    foreach ($locales_files as $lcfile) {
+        if (!$lcfile == "..") {
+            $lcdata = file_get_contents(LIGHTWEB_LOCALES_PATH . $lcfile);
+            file_put_contents(LIGHTWEB_PUBLISH_PATH . "uncompress/api/locales/" . $lcfile, $lcdata);
+        }
+    }
 }
