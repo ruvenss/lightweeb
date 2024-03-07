18d6b52870a92ee82ddee014c6bfcfc24106ef80
Ruvenss G. Wilches <ruvenss@gmail.com>
2024-03-07 02:40:42 +0100

    3.0

--- lightweb/publish.php
+++ lightweb/publish.php
@@ -8,12 +8,11 @@ function publish()
     echo "|-------------------------------------|----|\n";
     foreach (LIGHTWEB_TREE as $key => $value) {
         echo "| " . str_pad($key, 35, " ", STR_PAD_RIGHT) . " | ";
-
         echo "✅ |\n";
-        echo "--------------------------------------|----|";
+        echo "|-------------------------------------|----|";
         echo "\n";
     }
-    echo "--------------------------------------------\n";
+    echo "|------------------------------------------|\n";
     echo "|Render complete                           |\n";
     echo "--------------------------------------------\n";
 }
@@ -24,4 +23,11 @@ function prepare_render()
         mkdir(LIGHTWEB_PUBLISH_PATH . "compress");
         mkdir(LIGHTWEB_PUBLISH_PATH . "uncompress");
     }
+    if (!file_exists(LIGHTWEB_PUBLISH_PATH . "uncompress/api")) {
+        mkdir(LIGHTWEB_PUBLISH_PATH . "uncompress/api");
+    }
+    $htaccess = "RewriteEngine Off
+Options -Indexes
+ErrorDocument 404 https://" . LIGHTWEB_PRODUCTION . "/" . LIGHTWEB_LANG . "/404";
+    file_put_contents(LIGHTWEB_PUBLISH_PATH . "uncompress/.htaccess", $htaccess);
 }
