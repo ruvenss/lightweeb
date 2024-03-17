932ed6c9f9d5a17d3707d2340059e4653fe14de1
Ruvenss G. Wilches <ruvenss@gmail.com>
2024-03-17 23:10:24 +0100

    update

--- lightweb/init.php
+++ lightweb/init.php
@@ -4,17 +4,18 @@ if (LIGHTWEB_DB) {
     $ldb = new mysqli(LIGHTWEB_DB_HOST, LIGHTWEB_DB_USER, LIGHTWEB_DB_PASS, LIGHTWEB_DB_NAME);
     define("ldb", $ldb);
     if (ldb->connect_errno) {
-        die("LightWeb Failed to connect to DB Server: " . ldb->connect_error);
+        die ("LightWeb Failed to connect to DB Server: " . ldb->connect_error);
     }
 }
 if (!file_exists(dirname(dirname(__FILE__)) . "/api")) {
-    die(dirname(dirname(__FILE__)) . "/api is missing.");
+    die (dirname(dirname(__FILE__)) . "/api is missing.");
 }
 define("LIGHTWEB_URI", $_REQUEST);
-if (!isset(LIGHTWEB_URI['lang'])) {
+if (!isset (LIGHTWEB_URI['lang'])) {
     header("Location: /" . LIGHTWEB_LANG);
 }
-GetLanguages();
+if (!file_exists(LIGHTWEB_TREE))
+    GetLanguages();
 $uri = uripage();
 $fullpage = render_page($uri);
 echo i18n($fullpage);
--- lightweb/templates/config.php
+++ lightweb/templates/config.php
@@ -4,9 +4,9 @@
  * - development
  * - production
  */
-define('LIGHTWEB_ENVIRONMENT', 'development');
-define('LIGHTWEB_PRODUCTION', 'nizu.io');
-define('LIGHTWEB_STAGE', 'draft.nizu.io');
+define('LIGHTWEB_ENVIRONMENT', 'production');
+define('LIGHTWEB_PRODUCTION', '');
+define('LIGHTWEB_STAGE', '');
 define('LIGHTWEB_LANG', 'en');
 define('LIGHTWEB_VERSION', '3.0');
 define('LIGHTWEB_APIKEY', "My_secret_key");
@@ -25,25 +25,25 @@ define('LIGHTWEB_PAGES_FOOTERS_PATH', LIGHTWEB_PATH . 'lightweb/footers/');
 define('LIGHTWEB_PAGES_TEMPLATE_PATH', LIGHTWEB_PATH . 'lightweb/template/');
 define('LIGHTWEB_PUBLISH_PATH', LIGHTWEB_PATH . 'lightweb/publish/');
 if (!file_exists(LIGHTWEB_PAGES_PATH . 'tree.json')) {
-    file_put_contents(LIGHTWEB_PAGES_PATH . 'tree.json', '{}');
+    copy(LIGHTWEB_PATH . "templates/tree.json", LIGHTWEB_PAGES_PATH . 'tree.json');
 }
 if (!file_exists(LIGHTWEB_PAGES_PATH . 'siteconfig.json')) {
-    file_put_contents(LIGHTWEB_PAGES_PATH . 'siteconfig.json', '{}');
+    copy(LIGHTWEB_PATH . "templates/siteconfig.json", LIGHTWEB_PAGES_PATH . 'siteconfig.json');
 }
 define('LIGHTWEB_TREE', json_decode(file_get_contents(LIGHTWEB_PAGES_PATH . 'tree.json'), true));
 define('LIGHTWEB_SITE_CONFIG', json_decode(file_get_contents(LIGHTWEB_PAGES_PATH . 'siteconfig.json'), true));
 define('LIGHTWEB_DEBUG', false);
 define('LIGHTWEB_MINIFY', true);
-define('LIGHTWEB_DB', true);
-define('LIGHTWEB_DB_HOST', 'db.rgwit.be');
-define('LIGHTWEB_DB_USER', 'ruvenss');
-define('LIGHTWEB_DB_PASS', '1104');
-define('LIGHTWEB_DB_NAME', 'wpsocarbe');
-define('LIGHTWEB_DB_PREFIX', 'wp_');
+define('LIGHTWEB_DB', false);
+define('LIGHTWEB_DB_HOST', '');
+define('LIGHTWEB_DB_USER', '');
+define('LIGHTWEB_DB_PASS', '');
+define('LIGHTWEB_DB_NAME', '');
+define('LIGHTWEB_DB_PREFIX', '');
 define('LIGHTWEB_DB_PORT', '3306');
 define('LIGHTWEB_DB_CHARSET', 'utf8mb4');
 define('LIGHTWEB_DB_COLLATE', 'utf8mb4_unicode_ci');
 define('LIGHTWEB_NIZU_TOKEN', "");
 define('LIGHTWEB_NIZU_CMS', "");
-define('GOOGLE_UA', "XXXXXXX-X");
+define('GOOGLE_UA', "");
 define('FACEBOOK_PIXEL_ID', "");
