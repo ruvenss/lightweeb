acbe12917b3bafcde0e0b60248dd88eae651e640
Ruvenss G. Wilches <ruvenss@gmail.com>
2024-03-19 17:20:18 +0100

    master sample

--- api/my_config.php
+++ api/my_config.php
@@ -1,6 +1,13 @@
 <?php
 /**
  * Here you will define your own global variables
+ * Remember LIGHTWEB Globals starts by LIGHTWEB_ 
+ * you can allways use your own definitions
  */
 define("LIGHTWEB_SENDGRID", false);
 define("LIGHTWEB_SENDGRID_API_KEY", null);
+define("LIGHTWEB_ONESIGNAL_ID", null);
+define("LIGHTWEB_ONESIGNAL_API_KEY", null);
+define("LIGHTWEB_NIZUTOKEN", null);
+define("LIGHTWEB_NIZUCLOUDAPI", null);
+define("LIGHTWEB_STRIPE_SECRET", null);
