e2f6a41db090792f66819a62ab1bb94ac449f3d9
Ruvenss G. Wilches <ruvenss@gmail.com>
2024-04-01 15:36:01 +0200

    If this is undefined

--- lightweb/init.php
+++ lightweb/init.php
@@ -1,17 +1,17 @@
 <?php
 header("PoweredBy: LightWeb 3.0;");
-if (LIGHTWEB_DB) {
+if (LIGHTWEB_DB && !defined("ldb")) {
     $ldb = new mysqli(LIGHTWEB_DB_HOST, LIGHTWEB_DB_USER, LIGHTWEB_DB_PASS, LIGHTWEB_DB_NAME);
     define("ldb", $ldb);
     if (ldb->connect_errno) {
-        die ("LightWeb Failed to connect to DB Server: " . ldb->connect_error);
+        die("LightWeb Failed to connect to DB Server: " . ldb->connect_error);
     }
 }
 if (!file_exists(dirname(dirname(__FILE__)) . "/api")) {
-    die (dirname(dirname(__FILE__)) . "/api is missing.");
+    die(dirname(dirname(__FILE__)) . "/api is missing.");
 }
 define("LIGHTWEB_URI", $_REQUEST);
-if (!isset (LIGHTWEB_URI['lang'])) {
+if (!isset(LIGHTWEB_URI['lang'])) {
     header("Location: /" . LIGHTWEB_LANG);
 }
 if (!file_exists(LIGHTWEB_PAGES_PATH . "tree.json"))
