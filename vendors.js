a0f5398edbb13cf1f44fa3c7129958811b4bdc53
Ruvenss G. Wilches <ruvenss@gmail.com>
2024-04-28 12:38:39 +0200

    update

--- vendors.js
+++ vendors.js
@@ -125,7 +125,7 @@ function MyAlert(message) {
     console.info("Subscribed");
     alert("subscribed");
 }/* zxy_theme.js version [ 1710861754 ] */
-var LW_user_language = "{{lang_lc}}";
+var LW_user_language = "en";
 var LW_rel_ver = "86";
 if (localStorage.getItem("LW_user_language") === null) {
     localStorage.setItem("LW_user_language", LW_user_language);
