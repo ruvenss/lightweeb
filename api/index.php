b4eee6f5f315b6c77ecf3080d62657a8a2aa26f3
Ruvenss G. Wilches <ruvenss@gmail.com>
2024-06-11 19:28:08 +0200

    3.0.27

--- api/index.php
+++ api/index.php
@@ -1,12 +1,12 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-header("PoweredBy: LightWeb 3.0.0;");
+header("PoweredBy: LightWeb 3.0.27;");
 header("Content-Type: application/json; charset=UTF-8");
 define("errors", json_decode(file_get_contents("errors.json"), true));
-include_once("xhr_handler.php");
-include_once("lightweb.php");
-include_once("my_config.php");
-include_once("my_functions.php");
-include_once("my_init.php");
+include_once ("xhr_handler.php");
+include_once ("lightweb.php");
+include_once ("my_config.php");
+include_once ("my_functions.php");
+include_once ("my_init.php");
 
